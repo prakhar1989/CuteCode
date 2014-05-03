ecf0cc6e2e7e6798fe531e84b70a4d0ec3483e62
Prakhar Srivastav <prakhar1989@gmail.com>
2014-05-03 18:25:32 +0300

    Look re-vamp. Deleted extra cruft

--- cutecode/cutecode.go
+++ cutecode/cutecode.go
@@ -3,7 +3,6 @@ package cutecode
 import (
 	"appengine"
 	"appengine/datastore"
-	"appengine/user"
 	"html/template"
 	"net/http"
 	"time"
@@ -16,11 +15,8 @@ type Code struct {
 	UpdatedAt time.Time
 }
 
-//caching of templates
-var indexTemplate = template.Must(template.ParseFiles("templates/layout.html","templates/index.html"))
-var signinTemplate = template.Must(template.ParseFiles("templates/layout.html","templates/signin.html"))
-var codeformTemplate = template.Must(template.ParseFiles("templates/layout.html","templates/codeform.html"))
-var showcodeTemplate = template.Must(template.ParseFiles("templates/layout.html","templates/showcode.html"))
+var indexTemplate = template.Must(template.ParseFiles("templates/layout.html", "templates/index.html"))
+var showcodeTemplate = template.Must(template.ParseFiles("templates/layout.html", "templates/showcode.html"))
 
 //takes appengine Context and returns the datastore key
 func cutecodeKey(c appengine.Context) *datastore.Key {
@@ -28,12 +24,10 @@ func cutecodeKey(c appengine.Context) *datastore.Key {
 }
 
 func init() {
-	http.HandleFunc("/", handler)           //default handler
-	http.HandleFunc("/signin", userHandler) //if route param is signin then invoke userHandler
-	http.HandleFunc("/paste", codeHandler)  //if route param is paste then invoke codeHandler
-	http.HandleFunc("/save", saveHandler)   //if route param is save then invoke saveHandler
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/save", saveHandler)
+	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
 }
-
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
@@ -48,33 +42,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	err:=indexTemplate.ExecuteTemplate(w,"layout", codes) //this mention of layout stems from the {{layout}} of the layout page. for more http://blog.xcai.net/golang/templates
-	if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-    }
-}
-
-func userHandler(w http.ResponseWriter, r *http.Request) {
-	c := appengine.NewContext(r)
-	u := user.Current(c)
-	if u == nil {
-		url, err := user.LoginURL(c, r.URL.String())
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		w.Header().Set("Location", url)
-		w.WriteHeader(http.StatusFound)
-		return
-	}
-	err := signinTemplate.ExecuteTemplate(w, "layout", nil)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-}
-
-func codeHandler(w http.ResponseWriter, r *http.Request) {
-	err := codeformTemplate.ExecuteTemplate(w, "layout", nil)
+	err := indexTemplate.ExecuteTemplate(w, "layout", codes)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
@@ -93,7 +61,7 @@ func saveHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	templerr := showcodeTemplate.ExecuteTemplate(w,"layout", &code)
+	templerr := showcodeTemplate.ExecuteTemplate(w, "layout", &code)
 	if templerr != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
